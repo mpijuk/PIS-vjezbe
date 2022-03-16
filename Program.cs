e65fba129b6de0b4778bf8259c3f3fe6413c8103
petrapaz <73350450+petrapaz@users.noreply.github.com>
2022-03-16 17:53:32 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace vjezbaPIS1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Novo");
+            Console.WriteLine("Marioooo");
         }
     }
 }
